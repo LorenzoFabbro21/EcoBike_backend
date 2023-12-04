c4b3824a2f77dda077df64e81516be8889d77850
LorenzoFabbro21 <151538626+LorenzoFabbro21@users.noreply.github.com>
2023-12-04 15:27:22 +0100

    Add files via upload
    start with spring initialzr

---
+++ src/main/java/projectEcobike/ecobike_Backend/EcobikeBackendApplication.java
@@ -0,0 +1,13 @@
+package projectEcobike.ecobike_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcobikeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcobikeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projectEcobike/ecobike_Backend/EcobikeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package projectEcobike.ecobike_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcobikeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
